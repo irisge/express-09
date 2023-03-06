4a521b3b30ccdf877eb1bdd19704cebe956b6019
irisge <[iris.genevrier@gmail.com](mailto:iris.genevrier@gmail.com)>
2023-03-06 11:31:56 +0100

    upload and uuid

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,12 @@
 const express = require("express");
 
+const fs = require("fs");
+const multer = require("multer");
+
+const { v4: uuidv4 } = require("uuid");
+
+const upload = multer({ dest: "./public/uploads/" });
+
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
@@ -9,5 +16,18 @@ router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `./public/uploads/${filename}`,
+    `./public/uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 module.exports = router;
